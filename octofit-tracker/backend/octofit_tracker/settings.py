9e2469cd8dea5259ea76c0c4d453e911df3570e4
Kiran Vummaneni <71422952+kvummaneni@users.noreply.github.com>
2025-04-10 04:17:30 +0000

    Update API root to use codespace URL and allow host access

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-y%*+&mpdlerd)l9niyt+#9&8(&lx=5fze)dke*5c@85na(9&62
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'studious-chainsaw-xxg49575rwr26rg6-8000.app.github.dev']
 
 
 # Application definition
